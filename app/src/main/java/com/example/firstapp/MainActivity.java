09478e22c49376afc0264ffe938d3eb0067dd49f
tmstudent <tmstudentler@gmail.com>
2021-08-15 19:23:09 +0300

    finish

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView cardView, cardView2, cardView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardView = findViewById(R.id.cardClickOne);
+        cardView2 = findViewById(R.id.cardClickTwo);
+        cardView3 = findViewById(R.id.cardClickThree);
+
+        cardView.setOnClickListener(view ->
+                startActivity(new Intent(MainActivity.this, StepOneActivity.class))
+        );
+
+        cardView2.setOnClickListener(view ->
+                startActivity(new Intent(MainActivity.this, StepTwoActivity.class))
+        );
+
+        cardView3.setOnClickListener(view ->
+                startActivity(new Intent(MainActivity.this, StepThreeActivity.class))
+        );
+
     }
 }
